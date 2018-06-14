43305322b0af6dc7f48cdf685d8e6fd39f0fbcc0
Jaden Feddock <jaden.feddock@accenture.com>
2018-06-14 14:34:29 -0700

    Changed Keys

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var inMemoryStorage = new builder.MemoryBotStorage();
 
 // Creating the Bot
 const connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
 });
 const bot = new builder.UniversalBot(
     connector,
